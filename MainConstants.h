b7a7508abea6419a51ba994912b91fba72813913
mikri2017 <2007mik007@mail.ru>
2019-07-03 09:01:17 +0700

    Change game resolution (16:9)
    Сменил разрешение экрана (16:9)

--- MainConstants.h
+++ MainConstants.h
@@ -1,8 +1,8 @@
 #ifndef MAINCONSTANTS_H_INCLUDED
 #define MAINCONSTANTS_H_INCLUDED
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 540;
 constexpr double PI_by_180 = 3.14159265 / 180;
 
 enum gameReaction {
